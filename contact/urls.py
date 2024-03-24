daab9d390a533ab7c2f34e10a9a5388cb208ac8d
Iwerson <guilhermepdo2012@gmail.com>
2024-03-24 19:35:21 -0300

    pagina de criação de contact

--- contact/urls.py
+++ contact/urls.py
@@ -6,5 +6,7 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+    path('contact/create/', views.create, name='create')
 ]
