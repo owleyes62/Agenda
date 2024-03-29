8b8b83f1a11ef7c75d3f5591dcfeac27604d9204
Iwerson <guilhermepdo2012@gmail.com>
2024-03-29 04:11:16 -0300

    delete contatos (CRUD)

--- contact/urls.py
+++ contact/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
     path('<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
